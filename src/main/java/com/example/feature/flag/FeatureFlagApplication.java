4c2d0bd42d00e78c035de8b1677c5e89d920e2ce
Vaibhav992 <vaibhav200345@gmail.com>
2026-07-04 02:52:20 -0700

    feat: added flyway and db schema

--- src/main/java/com/example/feature/flag/FeatureFlagApplication.java
+++ src/main/java/com/example/feature/flag/FeatureFlagApplication.java
@@ -1,13 +1,22 @@
 package com.example.feature.flag;
 
+import java.util.Arrays;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+@Slf4j
 @SpringBootApplication
 public class FeatureFlagApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FeatureFlagApplication.class, args);
+		log.info("Starting Feature Flag Service");
+		ConfigurableApplicationContext context = SpringApplication.run(FeatureFlagApplication.class, args);
+		log.info(
+			"Feature Flag Service started. activeProfiles={}",
+			Arrays.toString(context.getEnvironment().getActiveProfiles())
+		);
 	}
 
 }
